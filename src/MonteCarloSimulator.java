14fe780c49516da99f606fd0d52eba98aca52f57
moshecmeli@gmail.com <moshe.carmeli@sonos.com>
2020-08-20 20:02:44 -0700

    Adding more info to printout

--- src/MonteCarloSimulator.java
+++ src/MonteCarloSimulator.java
@@ -10,24 +10,31 @@ public class MonteCarloSimulator {
     private static final Random randomGenerator = new Random();
     private static final Double INFLATION_RATE = 0.035;
 
+    /**
+     * Runs the simulation for each portfolio and then prints out the results
+     */
     public void comparePortfolios(int numberSimulations, int numberYears, double initialValue, Portfolio portfolio1, Portfolio portfolio2) {
         List<Double> portfolio1Results = runSimulation(numberSimulations, numberYears, initialValue, portfolio1.getMean(), portfolio1.getRisk());
         portfolio1.setLastSimulation(new SimulationResult(portfolio1Results, numberSimulations));
         Double median1 = portfolio1.getLastSimulation().getMedian();
         Double best10P1 = portfolio1.getLastSimulation().getTop10();
         Double worst10P1 = portfolio1.getLastSimulation().getWorst10();
-        System.out.println(String.format("Results for Portfolio1:\n\tMedian: %f\n\tBest 10: %f\n\tWorst 10: %f",
-                median1, best10P1, worst10P1));
+        System.out.println(String.format("Results for Portfolio1:\n\tMedian: %f\n\tBest 10: %f\n\tWorst 10: %f\n\tHistoric Risk: %.4f\n\tMean %.4f",
+                median1, best10P1, worst10P1, portfolio1.getRisk(), portfolio1.getMean()));
 
         List<Double> portfolio2Results = runSimulation(numberSimulations, numberYears, initialValue, portfolio2.getMean(), portfolio2.getRisk());
         portfolio2.setLastSimulation(new SimulationResult(portfolio2Results, numberSimulations));
         Double median2 = portfolio2.getLastSimulation().getMedian();
         Double best10P2 = portfolio2.getLastSimulation().getTop10();
         Double worst10P2 = portfolio2.getLastSimulation().getWorst10();
-        System.out.println(String.format("Results for Portfolio2:\n\tMedian: %f\n\tBest 10: %f\n\tWorst 10: %f",
-                median2, best10P2, worst10P2));
+        System.out.println(String.format("Results for Portfolio1:\n\tMedian: %f\n\tBest 10: %f\n\tWorst 10: %f\n\tHistoric Risk: %.4f\n\tMean %.4f",
+                median2, best10P2, worst10P2, portfolio2.getRisk(), portfolio2.getMean()));
     }
 
+    /**
+     * Performs the mathematical operations involved for each simulation.
+     * Goes through years desired and calculates new balance each year (inflation adjusted)
+     */
     private List<Double> runSimulation(int numberSimulations, int numberYears, Double initialValue, Double mean, Double risk) {
         return IntStream
                 .range(0, numberSimulations)
